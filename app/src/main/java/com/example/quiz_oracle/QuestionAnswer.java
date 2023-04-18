9712bb5af963a3fc5ee7fcd232236fdfc94fd47e
HR-04 <harinirengarajan203@gmail.com>
2023-04-18 23:04:47 +0530

    Quiz_App

---
+++ app/src/main/java/com/example/quiz_oracle/QuestionAnswer.java
@@ -0,0 +1,25 @@
+package com.example.quiz_oracle;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Which company owns the android?",
+            "Which one is not the programming language?",
+            "Where you are watching this Video?"
+
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Facebook","Whatsapp","Instagram","Youtube"}
+
+    };
+
+    public static String correctAnswers[] = {
+            "Google",
+            "Notepad",
+            "Youtube"
+    };
+
+}
